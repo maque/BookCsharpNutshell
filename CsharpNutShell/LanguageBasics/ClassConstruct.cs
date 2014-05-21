8137e5261b6319e4e020786789db6922f2157da1
maque <marcinmakowski@konto.pl>
2014-05-21 20:25:53 +0100

    Class bacisc and namespace added to solution and fixed

--- CsharpNutShell/LanguageBasics/ClassConstruct.cs
+++ CsharpNutShell/LanguageBasics/ClassConstruct.cs
@@ -1,27 +1,32 @@
-class ClassConstruct{
+class ClassConstruct
+{
 	//order is same for static and non-static fields
 	///static int x=y; static int y=3; x==0
 	private readonly int noChangableAfterConstruct = 10;
-	public const ConstValue = "Never Change!";//is evaluated
+	public const string ConstValue = "Never Change!";//is evaluated
 	public int Price;
 	private int Year;
-	static PropertyExample(){
+	static ClassConstruct()
+	{
 		//once per type
 	}
-	public int PropertyExample{
-		get { return Price;} 
-		set { Price = value;}
+	public int PropertyExample
+	{
+		get { return Price; }
+		set { Price = value; }
 	}
-	public int ReadOnlyProperty{
-		get { return Price * Year;}
+	public int ReadOnlyProperty
+	{
+		get { return Price * Year; }
 		//or private set {}
 	}
 	//compiled to get_XXX, set_XXX(value)
 	//
-	
-	public int this [int index]
+
+	public int this[int index]
 	{
-		get{
+		get
+		{
 			return Price;// cc[1]
 		}
 	}
@@ -31,9 +36,11 @@ class ClassConstruct{
 	///void Foo (ref int x) {...} // OK so far
 	///void Foo (out int x) {...} // Compile-time error
 	///
-	public ClassConstruct(){}
-	public ClassConstruct(int price){ Price = price;}
-	public ClassConstruct(int price, int year) : this(price) {
+	public ClassConstruct() { }
+	public ClassConstruct(int price) { Price = price; }
+	public ClassConstruct(int price, int year)
+		: this(price)
+	{
 		Year = year;
 	}
 	//Object initialzier
@@ -41,17 +48,33 @@ class ClassConstruct{
 	// optional parrameter is baked to new A("a","b","c")
 	// can be issue when referencing assembly, when changing constructor
 	// other call will produce default call
-	~ClassConstruct(){
+	~ClassConstruct()
+	{
 		//base.Finalize();
 	}
 }
 
-partial class PartialClass : ClassConstruct{}
-private class PartialClass{}
+partial class PartialClass : ClassConstruct { }
+partial class PartialClass { }
 
-public static void Display(ClassConstruct classConstruct);
-Display(PartialClass);
-public static value Display(PartialClass partialClass);
-Display(classConstruct)//compile time error
-	classConstruct = partialClass;
-	partialClass = (PartialClass)classConstruct;
+class TestPolyMorphysm
+{
+	public static void Display(ClassConstruct classConstruct)
+	{
+
+	}
+	public void DisplayTest()
+	{
+		Display(new PartialClass());
+		//DisplayPC(new ClassConstruct()); //compile time error
+	}
+	public void DisplayPC(PartialClass partialClass)
+	{
+		var classConstruct = new ClassConstruct();
+		partialClass = new PartialClass();
+		classConstruct = partialClass;
+		partialClass = (PartialClass)classConstruct;
+	}
+
+
+}
--- CsharpNutShell/LanguageBasics/NameSpaces.cs
+++ CsharpNutShell/LanguageBasics/NameSpaces.cs
@@ -1,59 +1,69 @@
-namespace Outer{
-	class Foo{}
-	
-	namespace Inner{
-		class Foo{}
-		class NameHiding{
+// csc /r:W1=Widgets1.dll /r:W2=Widgets2.dll application.cs
+extern alias W1;
+extern alias W2;
+
+namespace Conflicts
+{
+	class A
+	{
+		static void MainC()
+		{
+			System.Console.WriteLine(new A.B());
+			System.Console.WriteLine(new global::A.B());
+			//W1.Widgets.Widget w1 = new W1.Widgets.Widget();
+			//W2.Widgets.Widget w2 = new W2.Widgets.Widget();
+		}
+		class B { }
+	}
+
+}
+namespace A
+{
+	class B { }
+}
+
+
+namespace Outer
+{
+	class Foo { }
+
+	namespace Inner
+	{
+		class Foo { }
+		class NameHiding
+		{
 			Foo foo; // Outer.Inner.Foo
 			Outer.Foo foo2; //Outer.Foo
 		}
 	}
 }
 
-namespace Repeated{
-	class RepClass{}
+namespace Repeated
+{
+	class RepClass { }
 }
-namespace Repeated{
-	class RepClass2{}
+namespace Repeated
+{
+	class RepClass2 { }
 }
 namespace N1
 {
-class Class1 {}
+	class Class1 { }
 }
 namespace N2
 {
-using N1;
-class Class2 : Class1 {}
+	using N1;
+	class Class2 : Class1 { }
 }
+
 namespace N2
 {
-class Class3 : Class1 {} // Compile-time error
+	//class Class3 : Class1 {} // Compile-time error
+	using PropertyInfo2 = System.Reflection.PropertyInfo;
+	class Program { PropertyInfo2 p; }
+
+
 }
 
-aliasing
 
-using PropertyInfo2 = System.Reflection.PropertyInfo;
-class Program { PropertyInfo2 p; }
 
-// csc /r:W1=Widgets1.dll /r:W2=Widgets2.dll application.cs
-extern alias W1;
-extern alias W2;
-//allow use names
-{
-W1.Widgets.Widget w1 = new W1.Widgets.Widget();
-W2.Widgets.Widget w2 = new W2.Widgets.Widget();
-}
-//
-//
-namespace Conflicts{
-	class A{
-		static void Main(){
-	System.Console.WriteLine(new A.B());
-	System.Console.WriteLine(new global::A.B());
-		}
-	}
-	class B{}
-}
-namespace A{
-	class B{}
-}
